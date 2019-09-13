fe1fd4405826cbc48392dbc743bc0b25ffc2e117
Doug <me@douglasbowen.com>
2019-09-13 17:27:38 -0400

    almost working recursion

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,9 +6,18 @@ Your function must utilize recursion. It cannot contain any loops.
 
 #exit condition is when len(word) minus word.index is 2
 counter = 0
+stored_word = 'arthgyleth'
 def count_th(word):
-    if word.find('th') == -1:
-        return 0 
-    return word.find('th', count_th(word.find('th')+2))
+    if len(word) == 0:
+        return 0
+
+    foo = word.find('th')
+    if foo == 0:
+       
+        print(foo)
+        return count_th(word[2:])
+    else:
+        return count_th(word[foo:])
+   
+count_th('abcthefthghith')
     
-count_th('arthgyleth')
